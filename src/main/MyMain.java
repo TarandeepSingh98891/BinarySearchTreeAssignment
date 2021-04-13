d13a2d845fc86234960898f1b988cd0b4ba15310
TarandeepSingh98891 <tarandeep.bedi6@gmail.com>
2021-04-13 22:02:55 +0530

    Main method completed

--- src/main/MyMain.java
+++ src/main/MyMain.java
@@ -5,6 +5,18 @@ import implementation.MyBinarySearchTree;
 public class MyMain {
     public static void main(String[] args) {
         //create binary search tree
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        tree.insert(65);
+        tree.insert(52);
+        tree.insert(23);
+        tree.insert(14);
+        tree.insert(75);
+        tree.insert(92);
+        tree.inOrder(tree.getRoot());
+        tree.insert(53);
+        tree.inOrder(tree.getRoot());
+        System.out.println(tree.search(22));
+        System.out.println(tree.search(75));
         // and perform insert, search, traversal, delete
     }
 }
